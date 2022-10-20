1b93a1fe1b4e719b74355f82b3b48e39f9d14f1f
NourSoliman <noursoliman182@yahoo.com>
2022-10-21 00:26:43 +0200

    fixed icons on mobile screen

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebook , faGithub} from '@fortawesome/free-brands-svg-icons'
 const Footer = () => {
     return (
         <div className="footer">
             <div className="icons">
-                <a href="https://www.facebook.com/nour.sliman.18/" target="_blank"><i class="fa-brands fa-facebook" id="facebook"></i></a>
-                <a href="https://github.com/NourSoliman" target="_blank"><i class="fa-brands fa-github" id="github"></i></a>
+                <a href="https://www.facebook.com/nour.sliman.18/" target="_blank"><FontAwesomeIcon icon={faFacebook} className="facebook" id="icons"/></a>
+                <a href="https://github.com/NourSoliman" target="_blank"><FontAwesomeIcon icon={faGithub} className="github" id="icons"/></a>
             </div>
         </div>
     )
